dafb4a736e6f4ca370810d3b6749e8d30ed57788
LivBenedicto <livia.benedicto@hotmail.com>
2020-08-14 18:55:24 -0300

    Adding teacher form page

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+// style
+import './styles.css'
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Amazing! You want to teach!" />
+        </div>
+    )
+}
+
+export default TeacherForm;
