52f27a78d2c57a8e37a599ac317868ae945bf153
LivBenedicto <livia.benedicto@hotmail.com>
2020-08-17 14:56:12 -0300

    Adding tables (migrations)

---
+++ server/src/database/migrations/01_create_classes.ts
@@ -0,0 +1,23 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    // upgrade
+    return knex.schema.createTable('classes', table => {
+        table.increments('id').primary();
+        table.string('subject').notNullable();
+        table.decimal('cost').notNullable();
+
+        // foreing key
+        table.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+    });
+}
+
+export async function down(knex: Knex) {
+    // rollback
+    return knex.schema.dropTable('classes');
+}
